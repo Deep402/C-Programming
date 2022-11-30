1331dc3028b181ca6fc4212e17e3722ba24edc3b
Deep402 <115183567+Deep402@users.noreply.github.com>
2022-12-01 00:40:01 +0530

    Update add.c

--- C coding/Arithmetic Operators/add.c
+++ C coding/Arithmetic Operators/add.c
@@ -7,5 +7,5 @@ scanf("%d",&n1);
 printf("\n Enter Second Number : ");
 scanf("%d",&n2);
 sum = n1 + n2;
-printf("\n Summation is : %d",sum);
+printf("\n Summation is : %d",sum); // printf("\n Summation is : %d",n1 + n2);
 return 0;}
